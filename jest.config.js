fd78103a809d776f16df0e8a2f0a08e5776d746d
Fernando Canales Apu <110416342+fercanap@users.noreply.github.com>
2025-10-20 20:17:37 -0600

    Lower branch coverage threshold from 70 to 60

--- jest.config.js
+++ jest.config.js
@@ -9,7 +9,7 @@ module.exports = {
   testResultsProcessor: 'jest-junit',
   coverageThreshold: {
     global: {
-      branches: 70,
+      branches: 60,
       functions: 70,
       lines: 70,
       statements: 70,
